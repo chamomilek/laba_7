3c9edc1a7751f27acf1c65c53ca3d28c0dc9b387
Karina Osoprilko <chamomileekk@gmail.com>
2021-04-15 12:39:46 +0300

    first

---
+++ src/bsu/rfe/java/group10/lab7/Osoprilko/var13/MainFrame.java
@@ -0,0 +1,44 @@
+package bsu.rfe.java.group10.lab7.Osoprilko.var13;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
+    private static final int FRAME_MINIMUM_WIDTH = 500;
+    private static final int FRAME_MINIMUM_HEIGHT = 500;
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+    private static final int SMALL_GAP = 5;
+    private static final int MEDIUM_GAP = 10;
+    private static final int LARGE_GAP = 15;
+    private static final int SERVER_PORT = 4567;
+    private final JTextField textFieldFrom;
+    private final JTextField textFieldTo;
+    private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
+
+public class MainFrame {
+}
